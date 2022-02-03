29541eda8f61e86e1de126136ffbd64fbfa2ce45
Katelyn Lydeen <klydeen@hawk.iit.edu>
2022-02-03 12:34:43 -0600

    Initial Commit

---
+++ sin-x/sin_xApp.swift
@@ -0,0 +1,17 @@
+//
+//  sin_xApp.swift
+//  sin-x
+//
+//  Created by Katelyn Lydeen on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct sin_xApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
